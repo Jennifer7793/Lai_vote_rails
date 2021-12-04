1b38a1c164267e0495cd08e4d92fdab1bc5ef7eb
LAPTOP-56NU4RUS\jenni <jennifer7793@gmail.com>
2021-12-05 00:22:30 +0800

    CRUD_complete_v1

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -16,6 +16,25 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def edit
+    @candidate = Candidate.find_by(id:params[:id])
+  end
+
+  def update
+    @candidate = Candidate.find_by(id:params[:id])
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: 'Candidate Updated!'
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @candidate = Candidate.find_by(id:params[:id])
+    @candidate.destroy
+    redirect_to candidates_path, notice: "Candidate deleted!"
+  end
+
   private
   def candidate_params
     params.require(:candidate).permit(:name, :age, :party, :politics)
